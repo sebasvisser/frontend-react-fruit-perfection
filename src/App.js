5e9140413a3611a5c37a6f4a78ec54963f1bfe1c
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-09 23:08:04 +0200

    images geimporteerd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import Product from './Product';
 import './App.css';
+import citroenen from './assets/citroenen.jpeg';
+import limoenen from './assets/limoenen.png';
+import ijsblokjes from './assets/ijsblokjes.jpg';
+
 
 function App() {
     return (
         <main>
             <Product
-                image="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
+                image={citroenen}
                 title="Citroen"
                 description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
             />
             <Product
-                image="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
+                image={limoenen}
                 title="Limoen"
                 description="Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen."
             />
             <Product
-                image="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
+                image={ijsblokjes}
                 title="Ijsblokjes"
                 description="Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen."
             />
