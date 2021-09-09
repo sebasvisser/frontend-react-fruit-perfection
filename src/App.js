58769a9e3724a3574665414a6e5576e143a83b9d
sebasvisser <sebas.visser@mensenrelatie.nl>
2021-09-09 22:59:46 +0200

    update ivm codestyling

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ function App() {
         <article className="product">
             <img alt="Citroen" src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" />
             <h2 className="product-name">Citroen</h2>
-            <p className="product-description">Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.</p>
+            <p className="product-description">
+                Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.
+            </p>
         </article>
         <article className="product">
-            <img alt="Citroen" src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" />
+            <img alt="Limoen" src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" />
             <h2 className="product-name">Citroen</h2>
-            <p className="product-description">Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.</p>
+            <p className="product-description">
+               Limoen is familie van de citroen en de sinaasappel en behoort tot de citrusvruchten (Wijnruitfamilie). Limoenen zijn rond en kleiner dan citroenen. De schil is dun, vrij glad en groen.
+            </p>
         </article>
         <article className="product">
-            <img alt="Citroen" src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" />
+            <img alt="IJsblokjes" src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" />
             <h2 className="product-name">Citroen</h2>
-            <p className="product-description">Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.</p>
+            <p className="product-description">
+                Een ijsblokje of ijsklontje is bevroren water in de vorm van een klein blokje. Het wordt gemaakt in een diepvriezer door water in een plastic vorm te laten bevriezen.
+            </p>
         </article>
     </main>
   );
